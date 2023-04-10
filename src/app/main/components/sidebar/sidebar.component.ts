66fa2740f42c4855efa8d33200b3adeadeb128ca
garnicamatias <garnicamatias02@gmail.com>
2023-04-10 03:25:22 -0300

    guards in users components fixed

--- src/app/main/components/sidebar/sidebar.component.ts
+++ src/app/main/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteDialogComponent } from '../logout-dialog/logout-dialog.component';
 import { AdminGuard } from 'src/app/core/guards/admin.guard';
@@ -6,21 +6,29 @@ import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/r
 import { Observable } from 'rxjs';
 import { Session } from 'src/app/shared/models/session';
 import { SessionService } from 'src/app/core/services/session.service';
+import { User } from 'src/app/shared/models/user';
+import { LoginState } from 'src/app/login/state/login-state.reducer';
+import { Store } from '@ngrx/store';
+import { selectActiveUser } from 'src/app/login/state/login-state.selectors';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
 
-  session$ !: Observable<Session> 
-
+  session$ ?: Observable<User | undefined> 
+  
   constructor( 
     private dialog : MatDialog,
-    private session : SessionService
+    private loginStore : Store<LoginState>
     ){
-      this.session$ = this.session.getSession()
+  
+  }
+
+  ngOnInit(): void {
+    this.session$ = this.loginStore.select(selectActiveUser)
   }
 
   logout(){
